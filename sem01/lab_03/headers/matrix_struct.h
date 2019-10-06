8df9e8a3421b169f85d58cea894e0b18a105161b
Pavel Perestoronin <p_perestoronin@mail.ru>
2019-10-06 16:43:44 +0300

    created structure of the lab

---
+++ sem01/lab_03/headers/matrix_struct.h
@@ -0,0 +1,25 @@
+#ifndef _MATRIX_STRUCT_H_
+#define _MATRIX_STRUCT_H_
+
+#define MAX_SIZE_MATRIX 1000
+#define MAX_MATRIX_ELEMS 1000000
+
+typedef struct 
+{
+    int rows;
+    int columns;
+    int data[MAX_SIZE_MATRIX][MAX_SIZE_MATRIX];
+    
+} matrix_t;
+
+typedef struct
+{
+    
+} l_list_elem;
+
+typedef struct
+{
+    
+};
+
+#endif
