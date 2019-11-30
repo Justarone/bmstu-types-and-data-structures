905216fff7437ade745ecc3bb2db692fbdf31b8f
Pavel Perestoronin <p_perestoronin@mail.ru>
2019-11-30 15:26:32 +0300

    UPDATE: output function style

--- sem01/lab_05/functions/output_funcs.c
+++ sem01/lab_05/functions/output_funcs.c
@@ -6,12 +6,12 @@ void print_stat(const stat_t *const statistic)
 {
        printf("\n\nВремя работы системы: %lf\n", statistic->process_time);
        printf("Средняя длина очереди: %d\n", statistic->len_sum / statistic->check_num);
-       printf("Количество вошедших заявок: %d 1 типа и %d 2 типа\n", statistic->in_amount[TYPE_1],
+       printf("Количество вошедших заявок:\n%d 1 типа;\n%d 2 типа\n", statistic->in_amount[TYPE_1],
               statistic->in_amount[TYPE_2]);
-       printf("Количество вышедших заявок: \n%d 1 типа;\n%d 2 типа\n", statistic->out_amount[TYPE_1],
+       printf("Количество вышедших заявок:\n%d 1 типа;\n%d 2 типа;\n", statistic->out_amount[TYPE_1],
               statistic->out_amount[TYPE_2]);
-       printf("За это время машина:\nработала %lf секунд c 1 типом заявок,\nработала %lf секунд"
-              " c 2 типом заявок\nстояла %lf секунд\n",
+       printf("За это время машина:\nработала %lf секунд c 1 типом заявок;\nработала %lf секунд"
+              " c 2 типом заявок;\nстояла %lf секунд;\n",
               statistic->work_time[TYPE_1],
               statistic->work_time[TYPE_2],
               statistic->waiting_time);
